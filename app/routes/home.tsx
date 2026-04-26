b6ae7353c6352234ebe87d4d9b6ccc1fccb5bcef
Christian LDE <71846368+ldechristian@users.noreply.github.com>
2026-04-26 13:47:05 +0200

    Update meta tags for home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
 	return [
-		{ title: "New React Router App" },
-		{ name: "description", content: "Welcome to React Router!" },
+		{ title: "Host Kit - Open Source Software for a better Internet!" },
+		{ name: "description", content: "Contribution is accepted, please share your vision of the future of the Internet!" },
 	];
 }
 
